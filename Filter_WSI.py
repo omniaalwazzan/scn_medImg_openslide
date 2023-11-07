1e4a7a00fca34340dabce65f4a1c5d189305d098
Omnia Alwazzan <37316580+omniaalwazzan@users.noreply.github.com>
2023-11-07 12:51:24 +0000

    Update Filter_WSI.py

--- Filter_WSI.py
+++ Filter_WSI.py
@@ -1,52 +1,15 @@
-# This involves manual filtering as folders have mixed types of HCI and WSI, but this scrip helps in automize part of the manual selection 
+# Filter Image Step ONE: Moving images from nested folders
 import os
+import shutil
 from openslide import open_slide
 from PIL import Image
 import matplotlib.pyplot as plt
-# Path to the folder containing SCN images
-lis = os.listdir(r'D:\Guest-Collection_TM_QMUL_28_6_23\Batch 015')
-#lis = ['NH16-580', 'NH16-623', 'NH16-862']
-input_folder = r"D:\Guest-Collection_TM_QMUL_28_6_23\Batch 015" + '/' + lis[2] 
-#\NH13-1014
 
-i=0
-#input_folder =r"D:\Guest-Collection_TM_QMUL_28_6_23\Batch 005\NH08-210"
-#C:\Users\Omnia\Desktop\Batch 001\NH13-1256
-# Loop through the SCN files in the input folder
-for filename in os.listdir(input_folder):
-    if filename.endswith('.scn'):  # images are in SCN format
-        # Open the SCN file using openslide
-        scn_path = os.path.join(input_folder, filename)
-        slide = open_slide(scn_path)
-        
-        # Get the image thumbnail
-        slide_thumb_600 = slide.get_thumbnail(size=(1000, 3000))
-        
-        # Plot the thumbnail
-        plt.imshow(slide_thumb_600)
-        
-        # Plot the image name as a title
-        plt.title(filename)
-        
-        # Display the plot
-        plt.show()
-        
-        print(f"this is image {i+1}")
+# Batch number
+batch_number = "080"
 
-        print("\n --------------------\n")
-        i+=1
-
-
-##################################
-
-# Method TWO 
-
-# Filter Image Step ONE
-import os
-import shutil
-
-# Source directory containing nested folders with images
-source_directory = r"D:\Guest-Collection_TM_QMUL_28_6_23\Batch 027"
+# Path to the source directory
+source_directory = r"D:\Guest-Collection_TM_QMUL_28_6_23\Batch {}".format(batch_number)
 
 # Get the base folder name from the source directory
 base_folder_name = os.path.basename(source_directory)
@@ -76,17 +39,15 @@ for foldername, subfolders, filenames in os.walk(source_directory):
 
 print(f"Moved {moved_image_count} images to '{destination_directory}' successfully.")
 
+#       ###################################################################
+# Step TWO: Ploting imgs to know the stain type
 
-# Filter Image Step TWO
 # Path to the folder containing SCN images
-lis = os.listdir(r"D:\Phd\DNA_meth\dataset\img\Batch 027")
-#lis = ['NH16-580', 'NH16-623', 'NH16-862']
-input_folder = r"D:\Phd\DNA_meth\dataset\img\Batch 027/" 
-#\NH13-1014
+lis = os.listdir(r"D:\Phd\DNA_meth\dataset\img\Batch {}".format(batch_number))
+input_folder = r"D:\Phd\DNA_meth\dataset\img\Batch {}/".format(batch_number)
+
 
 i=0
-#input_folder =r"D:\Guest-Collection_TM_QMUL_28_6_23\Batch 005\NH08-210"
-#C:\Users\Omnia\Desktop\Batch 001\NH13-1256
 # Loop through the SCN files in the input folder
 for filename in os.listdir(input_folder):
     if filename.endswith(('.scn', '.ndpi')):  # images are in SCN format
@@ -95,7 +56,7 @@ for filename in os.listdir(input_folder):
         slide = open_slide(scn_path)
         
         # Get the image thumbnail
-        slide_thumb_600 = slide.get_thumbnail(size=(1000, 1000))
+        slide_thumb_600 = slide.get_thumbnail(size=(512, 512))
         
         # Plot the thumbnail
         plt.imshow(slide_thumb_600)
@@ -111,3 +72,5 @@ for filename in os.listdir(input_folder):
         print("\n --------------------\n")
         i+=1
 
+
+##################################
