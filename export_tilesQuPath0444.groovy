940322ab64f189e11c363ef3d4257ca502c2f26d
Omnia Alwazzan <37316580+omniaalwazzan@users.noreply.github.com>
2023-11-27 09:19:50 +0000

    Create export_tilesQuPath0444.groovy

---
+++ export_tilesQuPath0444.groovy
@@ -0,0 +1,49 @@
+/**
+ * Script to export image tiles (can be customized in various ways).
+ */
+
+
+def rois = getAnnotationObjects().collect {it.getROI()}
+def gson = GsonTools.getInstance(true)
+println gson.toJson(rois)
+
+// Get the current image (supports 'Run for project')
+def imageData = getCurrentImageData()
+
+// Define output path (here, relative to project)
+def name = GeneralTools.getNameWithoutExtension(imageData.getServer().getMetadata().getName())
+def pathOutput = buildFilePath(PROJECT_BASE_DIR, 'tiles', name)
+mkdirs(pathOutput)
+
+// Define output resolution in calibrated units (e.g. µm if available)
+double requestedPixelSize = 1.0
+
+// Convert output resolution to a downsample factor
+double pixelSize = imageData.getServer().getPixelCalibration().getAveragedPixelSize()
+double downsample = requestedPixelSize / pixelSize
+
+// Create an exporter that requests corresponding tiles from the original & labelled image servers
+new TileExporter(imageData)
+    .downsample(downsample)   // Define export resolution
+    .imageExtension('.tif')   // Define file extension for original pixels (often .tif, .jpg, '.png' or '.ome.tif')
+    .tileSize(512)            // Define size of each tile, in pixels
+    .annotatedTilesOnly(true) // If true, only export tiles if there is a (classified) annotation present
+    .overlap(1)              // Define overlap, in pixel units at the export resolution
+    .writeTiles(pathOutput)   // Write tiles to the specified directory
+
+extension = '.tif'
+
+def dirOutput = new File(pathOutput)
+for (def file in dirOutput.listFiles()) {
+    if (!file.isFile() || file.isHidden() || !file.getName().endsWith(extension))
+        continue
+    def newName = file.getName().replaceAll("=","-").replaceAll("\\[","").replaceAll("\\]","").replaceAll(",","_")
+    if (file.getName() == newName)
+        continue
+    def fileUpdated = new File(file.getParent(), newName)
+    println("Renaming ${file.getName()} ---> ${fileUpdated.getName()}")
+    file.renameTo(fileUpdated)
+}
+
+
+print 'Done!'
